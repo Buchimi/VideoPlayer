bef6a13a14c7849cba8b79e9b22ad8af36f0284c
Michael <61808599+killerbii@users.noreply.github.com>
2021-10-09 20:57:03 -0500

    Additions and subtractions

---
+++ src/App/Audioplayer.h
@@ -0,0 +1,8 @@
+#ifndef AUDIOPLAYER_H
+#define AUDIOPLAYER_H
+
+
+
+
+
+#endif // !AUDIOPLAYER_H
---
+++ src/Main/Video Player.cpp
@@ -0,0 +1,49 @@
+
+#include "SFML/Graphics.hpp"
+#include <iostream>
+#include "SFML/System/Vector2.hpp"
+
+#include "SFML/Audio/Music.hpp"
+using namespace std;
+int dosum(int a = 3, int b = 3) {
+    return a + b;
+}
+
+
+int main()
+{
+    sf::Music file; 
+
+    if (!file.openFromFile("C:/Users/sting/Downloads/Nightcore - Replay.wav")) {
+        cerr << "music not opened";
+    }
+    file.play();
+    
+    sf::RenderWindow window(sf::VideoMode(1200, 1200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    sf::Vector2f speed{ 1.0f, 1.0f };
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            if (event.type == sf::Event::KeyPressed) {
+                
+                if (event.key.code == sf::Keyboard::W) {
+                    shape.setPosition(shape.getPosition() + speed);
+                }
+            }
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
+
